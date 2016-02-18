3fd1b4289303a12931bf4cad6bc2e668012bb88f
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-02-18 19:38:38 +0000

    Add reportGenerator
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@7662 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- buildinfo/generateReport.py
+++ buildinfo/generateReport.py
@@ -15,44 +15,45 @@ pageHeader = '''<!DOCTYPE html>
 <script src="/build-info/IE9.js"></script>
 <![endif]-->
 </HEAD>
-<title>WiPro Staging Artifacts</title>'''
+<title> %s Artifacts Information</title>
+<body>
+<h1 style="text-align: center; color: black"> %s Artifacts Information</h1>
+'''
 
 tableHeader = '''
-<h1 style="text-align: center; color: black"> %s  Artifacts Information</h1>
-<body>
 <h3 align=center>Server:%s  Port:%s </h3>
 <table class="sortable" align=center>
 <tr><th>Application</th><th>Build Number</th><th>State</th><th>TargetName</th></tr>
 '''
 
-tableRow = '<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>'
+tableRow = '''<tr><td> %s </td><td> %s </td><td> %s </td><td> %s </td></tr>
+'''
 
-tableFooter = '''
-</table>
+tableFooter = '''</table>
 <br>
 '''
 
-pageFooter = '''</body>
+pageFooter = '''
+</body>
 </HTML>'''
 
 
-def genBody(userid, server, port, artifacts):
-	print tableHeader % (userid, server, port)
+def genBody(server, port, artifacts):
+	print tableHeader % (server, port),
 	for artifact in artifacts:
-		print tableRow % artifact
-	print tableFooter
+		print tableRow % artifact,
+	print tableFooter,
 
-def genReport(contents):
+def genReport(reportid, contents):
 	#print contents
-	print pageHeader
+	print pageHeader % (reportid, reportid)
 
 	for (adminurl, artifacts) in contents:
 		(protocol, server, port) = adminurl.split(':')
 		server = server.lstrip('/')
-		userid = server.split('-')[0] + port[:2]
-		genBody(userid, server, port, artifacts)
+		genBody(server, port, artifacts)
 
-	print pageFooter
+	print pageFooter,
 
 
 def _testReport():
@@ -67,7 +68,7 @@ def _testReport():
 
 	if len(sys.argv) == 2:
 		#print adminurl
-		artifacts = getArtifacts(adminurl, 'Deployment Monitor', 'yIHj6oSGoRpl66muev5S')
+		artifacts = getArtifacts(adminurl)
 	else:
 		user = sys.argv[2]
 		passwd = sys.argv[3]
@@ -75,8 +76,11 @@ def _testReport():
 		artifacts = getArtifacts(adminurl, user, passwd)
 
 	if artifacts:
+		(protocol, server, port) = adminurl.split(':')
+		server = server.lstrip('/')
+		userid = server.split('-')[0] + port[:2]
 		#print 'calling genReport'
-		genReport([(adminurl, artifacts)])
+		genReport(userid, [(adminurl, artifacts)])
 
 if __name__ == "main":
 	_testReport()
