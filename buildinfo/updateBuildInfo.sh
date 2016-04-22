0f427f2fc9cb08cae0d861c9e5b701e889b5cc5f
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-04-22 15:06:26 +0000

    Add runscript
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@8009 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

---
+++ buildinfo/updateBuildInfo.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Update here if JAVA or WebLogic is updated
+export JAVA_HOME=/cpg/3rdParty/installs/java/jdk1.7.0_80
+export WL_HOME=/cpg/3rdParty/installs/Oracle/Middleware_Home/wlserver_10.3
+
+
+# Setup the environment
+export CLASSPATH=${WL_HOME}/server/lib/weblogic.jar
+export PATH=${JAVA_HOME}/bin:${WL_HOME}/common/bin:${PATH}
+export WLST_PROPERTIES="-Dweblogic.security.TrustKeyStore=CustomTrust
+	-Dweblogic.security.CustomTrustKeyStoreFileName=/cpg/3rdParty/security/CPGTrust.jks
+	-Dweblogic.ThreadPoolPercentSocketReaders=75
+	-Dweblogic.ThreadPoolSize=32
+	-Dweblogic.security.allowCryptoJDefaultJCEVerification=true
+	-Dweblogic.security.allowCryptoJDefaultPRNG=true"
+
+# Determine the environment to run reports for
+PROFILE_DIR=/cpg/3rdParty/scripts/cpg/profiles
+CPG_ALIAS_LOOKUP_FILE=${PROFILE_DIR}/hostname.map
+CPG_HOSTNAME=$(egrep -i "^${HOSTNAME}," ${CPG_ALIAS_LOOKUP_FILE})
+CPG_ENV=${CPG_HOSTNAME%%-*}
+
+
+# Generate reports
+$(dirname ${0})/buildReports.py -e ${CGP_ENV} > buildReports.log
+
+# Copy reports to reporting server
+scp ${CGP_ENV}*.html optadm@10.131.237.93:/cpo/cpo_apps/build-info/wipro > scp.log
+
+# EOF
