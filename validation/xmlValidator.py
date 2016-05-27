2ded745826d4466891fe7f91706de731dd3070eb
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-05-27 16:07:14 +0000

    Program to validate xml files for syntax.
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@8119 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

---
+++ validation/xmlValidator.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+'''  Validate XML in files and directory trees
+'''
+
+
+import xml.dom.minidom
+from os import listdir
+from os.path import isfile, isdir, join
+from optparse import OptionParser
+import sys
+
+
+def validateFile(filename):
+	try:
+		xml.dom.minidom.parse(filename)
+		if options.verbose:
+			print filename, 'is valid'
+	except xml.parsers.expat.ExpatError as e:
+		print filename, e.message
+
+
+def validatePath(directory):
+	if options.verbose:
+		print 'Scanning directory', directory
+
+	for f in listdir(directory):
+		entry = join(directory, f)
+		#print 'Entry:', entry
+		if isfile(entry) and entry.endswith('.xml'):
+			validateFile(entry)
+
+
+def main():
+	global options
+	usage = "usage: %prog [options] filename|directory ..."
+
+	parser = OptionParser(usage=usage)
+	parser.add_option("-q", "--quiet",
+					  action="store_false", dest="verbose", default=False,
+					  help="don't print valid files or requested directories")
+	parser.add_option("-v", "--verbose",
+					  action="store_true", dest="verbose",
+					  help="print valid files and requested directories")
+
+	(options, args) = parser.parse_args()
+
+	for source in args:
+		print source
+		if isfile(source):
+			validateFile(source)
+		elif isdir(source):
+			validatePath(source)
+		else:
+			print "Invalid entry", source
+
+
+if __name__  == '__main__':
+	main()
+
+# EOF
