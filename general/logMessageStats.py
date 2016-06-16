cbf80f63d4f5594021962454b722571df002fb48
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-06-16 13:47:31 +0000

    Handle a full set of stacks; optimize regexs
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@8234 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- general/logMessageStats.py
+++ general/logMessageStats.py
@@ -10,32 +10,59 @@ import re
 
 
 linePatterns = [
-	'(\w*-?\w+) (ERROR|WARN):? \[\[\w+\].*\] (\S+)',
-	'<(Debug|Notice|Info|Warning|Error)>\s<(.*?)>.*<(.*)>',
+	' ([^<0-9]\S+) (ERROR) .+ (MissingProperty.+)>',
+	' ([^<0-9]\S+) (ERROR|WARN):? \[\[\w+\].+\] (\S.+?)(?: ?[-:\n(\?])',
+	' (ERROR) \S+ (\S+) .+?(File not found:.+?)>',
+	'^\S+ <(Error)> <(CpgIdentityJAASAsserterLogger)>.+(Failed to lookup customer profile)',
+	'^\S+ <(.+?)> <(.+?)>.+<(.?)>',
 ]
 
-# Patterns with no match groups will be ignored; use ?(...) for non-grouping
+# Patterns with no match groups will be ignored; use (?:...) for non-capture
 # Patterns with optional match groups will crash the program.
 exceptionPatterns = [
-	r'^\S+ <[^\n]*> <\S+ \S+ ([^<0-9]\S+) (ERROR|WARN).*?(^\S+).*? at (com\.?(cpc|pur)\S*?)\(([^)]+)',
-	r'^\S+ <[^\n]*> <\S+ \S+ ([^<0-9]\S+) (ERROR|WARN) \[\[\w+\].*?\] (\S+).*?(\S*)>',
-	r'^\S+ <[^\n]*> <\S+ \S+ ([^<0-9]\S+) (ERROR|WARN).*?(^\S+)',
-	r'^\S+ <[^\n]*> (ERROR|WARN).*?(^\S+).*? at (com\.?(cpc|pur)\S*?)\(([^)]+)',
-	r'^\S+ <[^\n]*> (ERROR|WARN).*?(^\S+).*?^uri :\[(\S*?)\]',
-	r'^\S+ <[^\n]*> (ERROR|WARN).*?(^\S+)',
-	r'^\S+ <[^\n]*> <\S+ \S+ \S+ INFO',
-	r'^\S+ <[^\n]*? (INFO).*?(^\S*)',
-	r'^\S+ <(Error)>.*?path:/(\S+).*?(^\S+).*? at (com\.?(cpc|pur)\..*?\(([^)]+))',
-	r'^\S+ <(Error).*?(^\S+).*?\s+at (weblogic\..*?\(([^)]+))',
-	r'^\S+ <Notice> <[^\n]*DefaultTimeBasedFileNamingAndTriggeringPolicy',
-	r'^\S+ <(Notice)> [^\n]*?(In|Out)bound Message',
-	r'^\S+ <(Notice)>.*?(^\S+).*? at (com\.?(cpc|pur)\..*?\(([^)]+))',
-	r'^\S+ <(Notice)>.*?/(\S+).*?(^\S+)',
-	r'^\S+ <(Notice)>.*?/(\S+).*?(^\S+)',
-	r'^\S+ <Notice> <[^\n]*\{svc=\w+, result=OK,',
-	r'^\S+ <Info> <JDBC> ',
-]
+	# Captured StdErr Messages
+	r'^\S+ <Notice> <(StdErr)> .+? (com\.hazelcast\..*)',
+	r'^\S+ <Notice> <(StdErr)> (?ms).+?(^SEVERE): \w+: (.+?)[>\n]',
+	r"^\S+ <Notice> <(StdErr)> (?ms).+?^(INFO|WARNING): .+? '(\w+\w+.+?)['@]",
+	r"^\S+ <Notice> <(StdErr)> (?ms).+?(^WARNING): .+? serviceName='(.+?)'",
 
+	# Single line matches
+	r'^\S+ <Notice> <.+? (INFO) \[\[\w+\].+?\] (\S+ (?:In|Out).+)',
+	r'^\S+ <Notice> <.+? (INFO) (\S+ - (?:In|Out)\S+ \S+)',
+	r'^\S+ <Notice> <.+? {svc=\w+, result=OK,',
+	r'^\S+ <Notice> <.+? c.q.l.core.rolling.DefaultTimeBasedFileNamingAndTriggeringPolicy',
+	r'^\S+ <Notice> <.+? (ERROR) .* (bad SQL grammar.+?\])',
+	r'^\S+ <Notice> <.+? (ERROR|WARN|INFO) \[\[\w+\].+?\] (.+?) \w+:',
+
+	# ui-cpo ERROR ...
+	r'^\S+ <Notice> <.+ ([^<0-9]\S+) (ERROR|WARN) (?ms).+?(^\S+).+?at (com\.(?:cpc|pur)\S+)',
+	r'^\S+ <Notice> <.+ ([^<0-9]\S+) (ERROR) \[\[\w+\].+?\] (com\S+)',
+	r'^\S+ <Notice> <.+ ([^<0-9]\S+) (ERROR) (?ms).+?(^\S.+?)[-\n(\?]',
+	r'^\S+ <(N)otice> <.+ ([^<0-9]\S+) (INFO) (?ms).+?(^\S+).+?at (com\.(?:cpc|pur)\S+)',
+	r'^\S+ <(No)tice> <.+ ([^<0-9]\S+) (WARN|INFO) \[\[\w+\].+?\] (com\S+)',
+	r'^\S+ <(Not)ice> <.+ ([^<0-9]\S+) (WARN|INFO) (?ms).+?(^\S.+?)[-:\n(\?]',
+
+	# Multiline matches ...
+	r'^\S+ <Notice> <.+? (ERROR) (?ms).*?(^\w\S+).+?at (com\.(?:cpc|pur)\S+)',
+	#r'^\S+ <(N)otice> <.+? (WARN|INFO) (?ms).*?(^\w\S+).+?at (com\.(?:cpc|pur)\S+)',
+	r'^\S+ <Notice> <.+? (ERROR) \S+ \S+ (site not (?ms)valid:).+?^uri :\[(\S+?)\]',
+	#r'^\S+ <(Not)ice> <.+ (WARN|INFO) (?ms).+?(^\w\S+).+?^uri :\[(\S*?)\]',
+
+	# Last ditch notice handlers
+	r'^\S+ <Notice> <.+ (ERROR) (?ms).+?^ExceptionMessage{.+?(^\w\S.+?)[-:\n(\?]',
+	#r'^\S+ <(Noti)ce> <.+ (WARN|INFO) (?ms).+?^ExceptionMessage{.+?(^\w\S.+?)[-\n(\?]',
+	r'^\S+ <Notice> <.+ (ERROR|WARN|INFO) (?ms).+?(^\w\S.+?)[-:\n(\?]',
+
+	# Error messages reported by WebLogic
+	r'^\S+ <(Error)> .+?path:/(\S+) (?ms).+?(^\S+).+?at (com\.(?:cpc|pur)\.\S+)',
+	r'^\S+ <(Error)> (?ms).+?(^\S+).+?at (weblogic\.\S+)',
+	r'^\S+ <(Error)> <(WebLogicServer)> .+? <\[(STUCK)\] (?ms).+?(^\w.+?)[-:\n(\?]',
+	r'^\S+ <(Warning)> <(JDBC)> .+? <(Forcibly releasing) inactive/harvested (connection) ".+?" .+? (".+?")',
+	r'^\S+ <Info> <JDBC>',
+
+	# Stray XML
+	r'^\s*(<.+?>)',
+]
 
 lineRegexes = []
 for pattern in linePatterns:
@@ -43,7 +70,8 @@ for pattern in linePatterns:
 
 exceptionRegexes = []
 for pattern in exceptionPatterns:
-	exceptionRegexes.append(re.compile(pattern,re.DOTALL + re.MULTILINE))
+	#print pattern
+	exceptionRegexes.append(re.compile(pattern))
 
 
 def processLine(line, stats):
@@ -69,6 +97,7 @@ def processException(lines, stats):
 	'''
 	key = ''
 	allLines= ''.join(lines)
+	re = None
 #	match = exceptionRe1.search(allLines)
 #	if match is not None:
 #		match = exceptionRe2.search(allLines)
@@ -76,6 +105,7 @@ def processException(lines, stats):
 	for regex in exceptionRegexes:
 		match = regex.search(allLines)
 		if match is not None:
+			re = regex
 			break
 
 	if match is not None:
@@ -83,6 +113,10 @@ def processException(lines, stats):
 			return
 
 		key = ' '.join(match.groups())
+		#if 'WARN' in  key:
+		#	print 'WAT key', key
+		#	print 'WAT pattern', regex.pattern
+		#	print
 
 		count, size = stats.get(key, (0, 0))
 		stats[key] = (count + 1, size + len(allLines))
