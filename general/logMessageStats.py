d1d3ef577a205d6870999f81dc5c99edf242fe17
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-06-20 21:38:32 +0000

    Adjust regexes
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@8245 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- general/logMessageStats.py
+++ general/logMessageStats.py
@@ -14,54 +14,59 @@ linePatterns = [
 	'^\S+ <Notice> <Std\w\w\w> .+? <LoggingService>',
 
 	# Stdout messages - filterd by domain
-	'^\S+ <Notice> <Stdout> <\S+> <a2\w2\dd1-c1.+? INFO',
-	'^\S+ <Notice> <(Stdout)> <\S+> <a[23].+? (ERROR|WARN|INFO) \[(?:\[\w+\])?.+?\] (.+?)[-0-9:=>]',
-	#'^\S+ <Notice> <(Stdout)> <\S+> <a[23]\w\d\dd1-c1.+? (ERROR) \[\S+\] (.+?)[):=>]',
-	'^\S+ <Notice> <(Stdout)> <\S+> <a[23]\w\d\dd1-c2.+? {(svc=\w+, result=\w+, operation=\w+),',
-	'^\S+ <Notice> <(Stdout)> <\S+> <a[23]\w\d\dd1-c2.+? (xmlns:env="http://schemas.xmlsoap.org/soap/envelope/")',
-	#'^\S+ <Notice> <(Stdout)> <\S> <a3\w3\dd1-c1.+?  (INFO)(from logger \w+),',
-	#'^\S+ <Notice> <(Stdout)> <\S+> <a3.+? (INFO) \[(?:\[\w+\])?.+?\] (\S+ \w+) \w+[-:=>]',
+	'^\S+ <Notice> <(Stdout)> <\S+> <a.+? ([a-z]\S+) (ERROR) \[\[\w+\].+?\] (MissingProperty.+)>',
+	'^\S+ <Notice> <(Stdout)> <\S+> <a1\w+-c1.+? ([a-z]\S+) (ERROR|WARN|INFO) \[(?:\[\w+\])?.+?\] (\S+)',
+	'^\S+ <Notice> <(Stdout)> <\S+> <a.+? (ERROR|WARN|INFO|DEBUG) \[(?:\[\w+\])?.+?\] (\S+)',
 	'^\S+ <Notice> <(Stdout)> <\S+> <a3\w3\dd1-c1.+? <(Drawing white:\w+)>',
-
+	'^\S+ <Notice> <(Stdout)> <\S+> <a[23]\w+-c2.+? {(svc=\w+, result=\w+, operation=\w+),',
+	'^\S+ <Notice> <(Stdout)> <\S+> <a[23]\w+-c2.+? (xmlns:env="http://schemas.xmlsoap.org/soap/envelope/")',
 
 	# Stdout messages
-	'^\S+ <Notice> <Stdout> .+? Fetching customer profile for CPCID',
-	'^\S+ <Notice> <Stdout> .+ WARN +\[(?:SearchLookup|InvalidationMonitor)\]',
-	'^\S+ <Notice> <(Stdout)> (ERROR) \S+ (\S+) .+?(File not found:.+?)>',
-	'^\S+ <Notice> <(Stdout)> ([^<0-9]\S+) (ERROR) .+ (MissingProperty.+)>',
-	'^\S+ <Notice> <(Stdout)> .+ ([^<0-9]\S+) (ERROR|WARN):? (?:\[\[\w+\].+\] )?(\S.+?)(?: ?[-:\n(\?])',
-	'^\S+ <Notice> <(Stdout)> .+ (WARN) +\[XSSFilter\] (.+?)>',
-	'^\S+ <Notice> <(Stdout)> .+ (c.c.n.u.t.c.TermsOfUseController \w+ \w+)',
-	'^\S+ <Notice> <(Stdout)> .+ <\S+ (ERROR|WARN) +\[\S+?\] (.+?)(?: ?[-:>]|code|name)',
-	'^\S+ <Notice> <Stdout> .+? <log4j: setFile',
+	#'^\S+ <Notice> <Stdout> .+? Fetching customer profile for CPCID',
+	'^\S+ <Notice> <(Stdout)> <.+? (WARN)  \[XSSFilter\] (.+?)>',
+	'^\S+ <Notice> <(Stdout)> <.+? (WARN)  \[\w+\] (\w+(?:[. ]\w+){,3})',
+	'^\S+ <Notice> <(Stdout)> <.+? \[\[\w+\].+?\] (ERROR|INFO |DEBUG) (\S+)',
+	#'^\S+ <Notice> <(Stdout)> (ERROR) \S+ (\S+) .+?(File not found:.+?)>',
+	'^\S+ <Notice> <(Stdout)> .+? ([a-z]\S+) (ERROR|WARN):? (?:\[\[\w+\].+\] )?(\S.+?)(?: ?[-:\n(\?])',
+	#'^\S+ <Notice> <(Stdout)> .+? (c.c.n.u.t.c.TermsOfUseController \w+ \w+)',
+	#'^\S+ <Notice> <(Stdout)> .+? <\S+ (ERROR|WARN) +\[\S+?\] (.+?)(?: ?[-:>]|code|name)',
 
 	# StdErr ouput
-	'^\S+ <Notice> <(StdErr)> .+? (\[IntroscopeAgent.Agent\] Exception \w+ing method tracer)',
-	'^\S+ <Notice> <(StdErr)> .+? (\[IntroscopeAgent.Agent\] Temporarily throttling the errors)',
+	'^\S+ <Notice> <(StdErr)> .+? (\[IntroscopeAgent.\w+\] \w+ \w+ \w+ \w+)',
 	'^\S+ <Notice> <(StdErr)> .+? <(line \d+:\d+ no viable .+?)>',
+	'^\S+ <Notice> <(StdErr)> .+? <Exception in thread "Thread-\d+" (.+?)>',
 
 	# Unhandled exceptions
-	'^\S+ <Notice> <(StdErr)> <.+? <(BEA-000000)> <(at) ',
-	'^\S+ <Notice> <(StdErr)> <.+? <(BEA-000000)> <(\S+)>',
-	'^\S+ <Notice> <(Std\w\w\w)> <.+? <((?!at )[^>]+)>\s*$',
+	'^\S+ <Notice> <(StdErr)> <.+? <(BEA-000000)> <(at )',
+	'^\S+ <Notice> <Std\w+> .+? <log4j: ',
 
 	# WebLogic Error Message
+	'^\S+ <(Alert)> <(WebLogicServer)> .+> <(\S+)>',
 	'^\S+ <(Error)> <(CpgIdentityJAASAsserterLogger)> .+ <BEA-000000> <(Failed to lookup customer profile)',
 	'^\S+ <(Error)> <(CpgIdentityJAASAsserterLogger)> .+ <BEA-000000> <(.+?: \w+)',
 	'^\S+ <(Error)> <(HTTP)> .+? <(Malformed Request ".+?")',
+	'^\S+ <(Warning)> <(HTTP)> .+? (class loader configuration ignored)',
+	'^\S+ <(Warning)> <(JNDI)> .+? (non-versioned global resource "\w+")',
+
 
 	# WebLogic Info messages
 	'^\S+ <(Info)> <(Common)> .+? <(Reached maximum capacity of pool "\w+")',
-	#'^\S+ <(Info)> <(Cluster)> .+? <(Lost \d+ unicast message\(s\))',
+	'^\S+ <(Info)> <(Cluster)> .+? <(Lost \d+ unicast message\(s\))',
 	#'^\S+ <(Info)> <(Common)> .+? <(Created "\d+" resources for pool "\w+")',
 	#'^\S+ <(Info)> <(JDBC)> .+? (pool ".+?") has been (closed)',
-	#'^\S+ <(Info)> <(WorkManager)> .+? M(maximum thread constraint ClusterMessaging is reached)',
-	'^\S+ <Info>',
+	'^\S+ <(Info)> <(WorkManager)> .+? M(maximum thread constraint ClusterMessaging is reached)',
+	'^[^>]+> <Info> ',
+	'^[^>]+> <Notice> <(?:Cluster|Log Management|Security|Server|WebLogicServer)>',
 
 	# WebLogic Warnings
 	'^\S+ <(Warning)> <(Socket)> .+? <(Closing socket as no data read)',
 	'^\S+ <(Warning)> <(CpgIdentityJAASAsserterLogger)> .+ <BEA-000000> <(.+?: \w+)',
+	'^[^>]+> <(Warning)> <(Management)> .+> <(.+)>',
 
+	# Debug Messages
+	'^\S+ <(\w)> <(\w)> .+? (DEBUG)',
+
+	#'^[^>]+> <Notice> <(Std\w+)> <.+ <[^<]+)>',
 ]
 
 # Patterns with no match groups will be ignored; use (?:...) for non-capture
@@ -76,26 +81,36 @@ exceptionPatterns = [
 	r'^\S+ <Notice> <(StdErr)> (?ms).+?com\.sun\.xml\..+?$\s(SEVERE): \w+: (.+?)$',
 	r"^\S+ <Notice> <(StdErr)> (?ms).+?^(INFO|WARNING): .+? '(\w.+?)['@]",
 	r"^\S+ <Notice> <(StdErr)> (?ms).+?(^WARNING): .+? serviceName='(.+?)'",
-
-	# Captured StdOut Messages
-	r'^\S+ <Notice> <Stdout> <.+? c.q.l.core.rolling.',
-	r'^\S+ <Notice> <(Stdout)> <.+? {(svc=\w+, result=OK, operation=\w+),',
-	r'^\S+ <Notice> <(Stdout)> <.+? (INFO) \[\[\w+\].+?\] (\S+ (?:In|Out).+)',
-	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) .* (bad SQL grammar.+?\])',
-	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR|WARN|INFO) +\[\[\w+\].+?\] (\S+)',
+	r'^\S+ <Notice> <(StdErr)> (?ms)<.+?$\s?(INFO)',
+	r'^\S+ <Notice> <(StdErr)> (?ms)<.+?$\s(SEVERE|ERROR|WARN).+?(^\S+):.+?at ((:?com\.cpc|cpdt)\.\S+)',
+	r'^\S+ <Notice> <(StdErr)> (?ms)<.+?$\s(SEVERE|ERROR|WARN):\s*?$.+?^(\S+.+?)$',
+	r'^\S+ <Notice> <(StdErr)> <.+ (\w+ of missing type cpdt\.\w+\.rules\.)',
 
 	# ui-cpo ERROR ...
 	r'^\S+ <Notice> <Stdout> <.+ ([^<0-9]\S+) (ERROR|WARN) (?ms).+?(^\S+):.+?at (com\.(?:cpc|pur)\S+)',
 	r'^\S+ <Notice> <Stdout> <.+ ([^<0-9]\S+) (ERROR) \[\[\w+\].+?\] (\S+)',
 	r'^\S+ <Notice> <Stdout> <.+ ([^<0-9]\S+) (ERROR) (?ms).+?(^\S.+?)[-\n(\?]',
 
+	# Captured StdOut Messages
+	r'^\S+ <Notice> <(Stdout)> <.+? {(svc=\w+, result=OK, operation=\w+),',
+	r'^\S+ <Notice> <(Stdout)> <.+? (INFO) \[\[\w+\].+?\] (\S+ (?:In|Out).+)',
+	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) .* (bad SQL grammar.+?\])',
+	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR|WARN|INFO) +\[\[\w+\].+?\] (\S+)',
+	r'^\S+ <Notice> <Stdout> <.+? c\.q\.l\.co(?:re)?\.rolling\.',
+	r'^\S+ <Notice> <(Stdout)> .+?(FATAL).+?(\w+).+ (\S+: [^:]+)',
+	r'^\S+ <Notice> <(Stdout)> <\S+> <a3\w3\dd1-c1.+? <(Drawing white:\w+)',
+
+
 	# Multiline matches ...
-	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) (?ms).*?(^\w\S+).+?at (com\.(?:cpc|pur)\S+)',
+	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) (?ms).*?(^\w\S+).+?at ((?:com\.(?:cpc|pur)|cpdt\.)\S+)',
 	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) \S+ \S+ (site not (?ms)valid:).+?^uri :\[(\S+?)\]',
+	r'^\S+ <Notice> <(Std...)> <.+? (?ms).+(PreparedStatementCallback); SQL \[(.+?)\]; (.+?)>?$',
 
 	# Last ditch notice handlers
+	r'^\S+ <Notice> <(Stdout)> <.+? \[\[\w+\].+?\] (ERROR|INFO |DEBUG) (\S+)',
 	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR) (?ms).+?^ExceptionMessage{.+?(^\w\S.+?)[-:\n(\?]',
 	r'^\S+ <Notice> <(Stdout)> <.+? (ERROR|WARN|INFO) (?ms).+?(^\w\S.+?)[-:\n(\?]',
+	r'^\S+ <Notice> <(StdErr)> <.+? <(Nested (?ms)exception:).+?^(\w[^:]+)',
 
 	# Error messages reported by WebLogic
 	r'^\S+ <(Error)> <(HTTP)> .+?path:/(\S+) (?ms).+?(^\S+?)\s.+?at .+?at (com\.(?:cp|pur)\S+)',
@@ -103,8 +118,9 @@ exceptionPatterns = [
 	r'^\S+ <(Error)> <(Kernel)> (?ms).+?(^\S+).+?at (weblogic\.\S+)',
 	r'^\S+ <(Error)> <(Kernel)> (?ms).+?(^\S+).+?at (weblogic\.\S+)',
 	r'^\S+ <(Error)> <(WebLogicServer)> .+? <\[(STUCK)\] (?ms).+?(^\w.+?)[-:\n(\?]',
+	r'^\S+ <(Error)> <(WebLogicServer)> .+? <\[(STUCK)\] (?ms).+?(com\.(?:cp|pur)\S+)',
 	r'^\S+ <(Error)> <ServletContext-/(.+?)> (?ms).+?(^\S+).+?at (weblogic\.\S+)',
-	r'^\S+ <(Error)> <ServletContext-/(cpotools)> .+?path:/(\S+) (?ms).+?(^\S+).+?at (com\.(?:cp|pur)\.\S+)',
+	r'^\S+ <(Error)> <ServletContext-/(cpotools)> .+?path:/(\S+) (?ms).+?(^\S+).+?at (com\.(?:cp|pur)\S+)',
 	r'^\S+ <(Warning)> <(JDBC)> .+? <(Forcibly releasing) inactive/harvested (connection) ".+?" .+? (".+?")',
 
 	# Startup notices
@@ -120,6 +136,7 @@ exceptionPatterns = [
 	#r'^\S+ <Info> <RJVM> .+? Network Configuration',
 	#r'^\S+ <Info> <WebLogicServer> .+? WebLogic Server ".+?" version:',
 	#r'^\S+ <Info> <WorkManager> .+? Initializing self-tuning thread pool',
+	r'^[^>]+> <(?:Info|Notice)> <(?:Management|WebLogicServer)>',
 
 	# Stray XML
 	r'^\s*(<.+?>)',
@@ -127,13 +144,17 @@ exceptionPatterns = [
 
 lineRegexes = []
 for pattern in linePatterns:
-	#print pattern
-	lineRegexes.append(re.compile(pattern))
+	try:
+		lineRegexes.append(re.compile(pattern))
+	except Exception:
+		print 'invalid:', pattern
 
 exceptionRegexes = []
 for pattern in exceptionPatterns:
-	#print pattern
-	exceptionRegexes.append(re.compile(pattern))
+	try:
+		exceptionRegexes.append(re.compile(pattern))
+	except Exception:
+		print 'invalid:', pattern
 
 
 def processLine(line, stats):
@@ -251,13 +272,11 @@ def reportStats(stats):
 
 def main():
 
-	usage= '''
-usage: %prog [options] file [file...]
+	usage= '''%prog [options] file [file...]
 
 Sumarizes one or more log files reporting counts and size (bytes):
  - log messages
- - stack dumps by cpc classes
-'''
+ - stack dumps by cpc classes'''
 
 	parser = OptionParser(usage=usage)
 	(options, args) = parser.parse_args()
@@ -268,7 +287,7 @@ Sumarizes one or more log files reporting counts and size (bytes):
 	else:
 		print "FATAL: no files specified"
 		parser.print_help()
-
+		exit()
 
 	if stats:
 		reportStats(stats)
