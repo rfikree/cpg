e429240eacd2f56f94c8cf83b1c2e66fb38c3844
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2016-12-15 18:44:27 +0000

    Change upload behaviour
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@8762 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- utils/zip_etc.sh
+++ utils/zip_etc.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 # Zip critical files in /etc and attempt to upload to jscape server
+# uploads onlit if user us specified
 
-# Usage /cpg/3rdParty/scripts/cpg/utils/zip_etc.sh [user|'' [scp_host]]
+# Usage /cpg/3rdParty/scripts/cpg/utils/zip_etc.sh [user [scp_host]]
+user=${1}
 
 # Make zip readable
 umask 022
@@ -69,9 +71,9 @@ fi
 
 # Upload the file
 
-if [[ -n ${host} ]]; then
-	echo Uploading to ${1}${1:+@}${host}:Solaris
-	scp -P2022 ${ZIP_FILE}  ${1}${1:+@}${host}:Solaris && rm ${ZIP_FILE}
+if [[ -n ${user} ]]; then
+	echo Uploading to ${user}@${host}:Solaris
+	scp -P2022 ${ZIP_FILE}  ${user}@${host}:Solaris && rm ${ZIP_FILE}
 fi
 
 echo Done
