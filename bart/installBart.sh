09b4341513c07ac7d986fd78a6db6c97dc336bf2
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2017-10-27 00:23:02 +0000

    Initial bart checkin
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@10280 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

---
+++ bart/installBart.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+
+INSTALL_BASE=/usr/local
+SOURCE_BASE=/cpg/3rdParty/scripts/cpg/bart
+BART_MANIFESTS=/var/tmp
+
+CPG_ALIAS_LOOKUP_FILE=${PROFILE_DIR}/hostname.map
+CPG_HOSTNAME=$(egrep -i "^$(hostname)," ${CPG_ALIAS_LOOKUP_FILE})
+export CPG_HOSTNAME=${CPG_HOSTNAME##*,}
+
+
+# Function to update or install configs only if missing or changed
+#    Params: source destination [cp_options]
+updateChanged() {
+	if [ ! -f ${1} ]; then
+		echo FATAL: Source file ${1} is missing
+		exit 1
+	elif [ ! -f ${2} ]; then
+		echo Installing: ${2}
+		cp ${3} ${1} ${2}
+		return 0
+	elif ! diff -q ${1} ${2} >/dev/null; then
+		echo Updating: ${2}
+		cp ${2}{,.$(date +%Y%m%dT%H%M)}
+		cp ${3} ${1} ${2}
+		return 0
+	fi
+	return 1
+}
+
+
+# Function to update or install files only if missing or changed
+installFiles() {
+}
+
+
+# Define the rules file to apply; check for unsupported server
+rules_file=bart.rules
+case ${CPG_HOSTNAME:-''} in
+        *-appadm)
+                ;;
+        *-bdt)
+                ;;
+        *-blcpo)
+                 ;;
+        *-cpodeploy)
+        		rules_file=bart.rules.cpodeploy
+                ;;
+#        *-soaz0)
+#                ;;
+#        *-soaz1)
+#                ;;
+        *-uicpo)
+                ;;
+        *-wladm)
+                ;;
+        *-ws)
+                ;;
+        *)
+        		echo Unknown host ${HOSTNAME} ${CPG_HOSTNAME}
+        		exit 99
+        		;;
+esac
+
+
+# Install the files
+updateChanged ${SOURCE_BASE}/${1} ${INSTALL_BASE}/etc/bart.rules
+updateChanged ${SOURCE_BASE}/${1} ${INSTALL_BASE}/sbin/bartlog
+
+
+# Remove script from bin if found
+if [ -f ${INSTALL_BASE}/bin/bartlog ]; then
+	rm ${INSTALL_BASE}/bin/bartlog
+fi
+
+
+# EOF
