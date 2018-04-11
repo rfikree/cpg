90c3916d0973eb946b1dcf3a36427450f547c57d
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2018-04-11 19:25:04 +0000

    Extend functionality to other log formats
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@10816 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- general/logstats.py
+++ general/logstats.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 '''
-This program generates statistics from WebLogic access logs.
+This program generates statistics from WebLogic or other access logs.
 Log files should be in the standard CPO format and may be gzipped.
-Parameters are removed from the URL and URLs are summarized by pattern.
+Parameters are removed from the path and paths are summarized by pattern.
 
 After each file is parsed a line with the name of the parsed file
-and the current number of url patterns is printed.
+and the current number of path patterns is printed to stderr.
 
-URLS are shortened to remove parameters, and session ids.  Further shortening
+Files can be gzipped.
+
+Paths are shortened to remove parameters and filenames. This
 is done to reduce the output list size.
 
 Options:
     --help:  Print this usage
     --output (-o) <file>: Output report to specified file (default stdout)
-    --hits (-h) <hits> : Min hits to report line (default 100)
-    --time (-t) <seconds>:  Report all URL patterns with a hit over this time.
-    --contents (-c) <urlSubstring>: Report only URLs containing string
-    --regex (-r) <regex>: Report only if URL matches regex
+    --hits (-h) <hits> : Min hits to report line (default: 100)
+    --time (-t) <seconds>:  Report only path patterns which exceeded time.
+    --contents (-c) <pathSubstring>: Report only paths containing string
+    --regex (-r) <regex>: Report only if path matches regex
+    --format (-f) [1,2,3]: Select format to use (default: 1)
+    --limits: Report limits (min, median, 95%, max) for response times
 
 Output includes
   - count: number of hits for the specified pattern
   - mean response time with standard devation
-  - response times (10%, avg, 90%, and 95%)
+  - response times (10%, median/50%, 90% and 95% percentiles)
   - the pattern the preceeding statistics apply to
+
+Format values:
+  1 - WebLogic
+  2 - Nginx reverse proxy
+  3 - WebSeal
 
 '''
 #  - response times (min, avg, 90%, and max)
@@ -43,27 +52,45 @@ def openLogFile(fileName):
         return open(fileName, 'r')
 
 
-def parseLog(fileName, data, contents, regex):
+def parseLog(fileName, data, contents, regex, format):
     ''' parse a named access log file updating
         statistics dictionary "data" is updated for each row
     '''
+
+    divisor = 1
+    if format == 1:
+        pattern = r'\s(?P<time>\d+\.\d+)\s[^/]*(?<=\s)(?P<path>/[^ \t?;]*)[ \t?;]'
+    elif format == 2:
+        pattern = r'\s(?P<path>/[^ \t?;]*)[ \t?;].*?\s(?P<time>\d+\.\d+)\s'
+    elif format == 3:
+        pattern = r'//[^/]*(?P<path>/[^ \t?;]*)[ \t?;].*\s(<time>\d{9}|range)\s'
+        divisor = 1000000
+    else:
+        print('Invalid format specified - Exiting')
+        sys.exit(-1)
+
+    statsFeilds = re.compile(pattern)
     try:
         #print 'parseLog', fileName, len(data)
         for line in openLogFile(fileName):
             #print( 'parseLog', line[:-2] )
             if line[0] == '#':
                 continue
-            fields = line.split('\t')
-            if len(fields) != 9:
+            fields = statsFeilds.search(line)
+            if not fields:
                 continue
-            (ip, aDate, aTime, method, status, bytes,
-                timeTaken, uri, userAgent) = fields
-            if contents is not None and contents not in uri:
+            timeTaken = fields.group('time')
+            path = fields.group('path')
+            #print( 'parseLog', timeTaken, path)
+            if contents is not None and contents not in path:
                 continue
-            if regex is not None and not regex.search(uri):
+            if regex is not None and not regex.search(path):
                 continue
-            key = parseURI(uri)
-            timeTaken = float(timeTaken)
+            key = parsePath(path)
+            try:
+              timeTaken = float(timeTaken) / divisor
+            except ValueError:
+                timeTaken = 120.0
             #print( 'parseLog', timeTaken, key )
             if key in data:
                 data[key].extend((timeTaken,))
@@ -77,57 +104,54 @@ def parseLog(fileName, data, contents, regex):
 
     return data
 
-def parseURI(uri):
-    ''' parse a URI converting to standard format.
-        returning a key (URL pattern)
+def parsePath(path):
+    ''' parse a path converting to standard format.
+        returning a normalized path (pattern)
     '''
     allUpperCase = re.compile('[-A-Z0-9._@]+$')
-    fileExt = re.compile('[^.]+\.(?!page)\w+$')
-    urlChanged = False
+    fileExt = re.compile('[^.]+\.(?!page|jsf)\w+$')
+    pathChanged = False
 
-    #print( 'parseLine', timeTaken, uri )
-    key = uri.split('?')[0].split(';')[0]
-    parts = key.split('/')
+    parts = path.split('/')
 
-    # Replace numeric or uppercase URL componentss (/rs)
+    # Replace numeric or uppercase path componentss (rest services, etc.)
     for i in range(1, len(parts)):
-        #if allDigits.match(parts[i]) or allUpperCase.match(parts[i]):
-        if allUpperCase.match(parts[i]):
+         if allUpperCase.match(parts[i]):
             parts[i] = '*'
-            urlChanged = True
-        #elif upperCaseExt.match(parts[i]):
-        #	parts[i] = parts[i].split('.')[0] + '.*'
-        #	urlChanged = True
+            pathChanged = True
 
-    #  Replace filename like component at end of URL
+    #  Replace filename like component at end of path
     if fileExt.match(parts[-1]):
         parts[-1] = '*.' + parts[-1].split('.')[-1]
-        urlChanged = True
+        pathChanged = True
 
-    if urlChanged:
-        key = '/'.join(parts)
-    return key
+    if pathChanged:
+        path = '/'.join(parts)
+    return path
 
-def reportData(data, output, hits, rptTime):
+def reportData(data, output, hits, rptTime, limits):
     ''' Report the access data to the specified ouput file.
         Only report data if count exceeds hits.
-        If rptTime is specified, report the data if a hit exceeds the hit time.
+        If rptTime is specified, only report the data if a hit exceeds the limit.
         Data dictionary contains an array of hits for the key
     '''
-    print ('%8s%7s%6s%7s%6s%6s%7s  %s' % (
-        'Count', 'Mean', 'SDev', '10%', 'Med', '90%', '95%', 'URL Pattern'),
-        file=output)
-#		'Count', 'Mean', 'SDev', 'Min', 'Med', '95%', 'Max', 'URL Pattern'),
+    if limits:
+        hdr = ('Count', 'Mean', 'SDev', 'Min', 'Med', '95%', 'Max', 'Path Pattern'),
+    else:
+        hdr = ('Count', 'Mean', 'SDev', '10%', 'Med', '90%', '95%', 'Path Pattern')
+    print ('%8s%7s%6s%7s%6s%6s%7s  %s' % hdr, file=output)
+
     for key in sorted(data.iterkeys()):
         if  len(data[key]) > hits or rptTime:
             (count, mean, stdDev, minTime, p5, p10, median, p90, p95, maxTime) = \
                 genStats(data[key])
         if len(data[key]) > hits \
-        or (rptTime and maxTime > rptTime) :
-            print ('%8d%7.2f%6.2f%7.2f%6.2f%6.2f%7.2f  %s' % (
-                count,  mean, stdDev, p10, median, p90, p95, key),
-                file=output)
-#				count,  mean, stdDev, minTime, median, p95, maxTime, key),
+        and (not rptTime or maxTime > rptTime):
+            if limits:
+                cols = (count, mean, stdDev, minTime, median, p95, maxTime, key)
+            else:
+                cols = (count, mean, stdDev, p10, median, p90, p95, key)
+            print ('%8d%7.2f%6.2f%7.2f%6.2f%6.2f%7.2f  %s' % cols, file=output)
 
 def genStats(timeData):
     ''' Generate statistics from a series of time values
@@ -151,7 +175,7 @@ def genStats(timeData):
     p10 = sortedData[count/10]
     p90 = sortedData[-count/10]
     p95 = sortedData[-count/20]
-    p99 = sortedData[-count/100]
+    #p99 = sortedData[-count/100]
 
     return (count, mean, stdDev, minTime, p5, p10, median, p90, p95, maxTime)
 
@@ -167,22 +191,32 @@ def main():
     '''
 
     # Option defaults
+    format = 1
     hits = 100;
+    limits = False
     output = sys.stdout
     outputSet = False
     regex = None
     rptTime = None
-    urlContents = None
+    pathContents = None
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'c:h:o:r:t:',
-            ['help', 'contents=', 'hits=', 'output=', 'regex=', 'time='])
+        opts, args = getopt.getopt(sys.argv[1:], 'c:f:h:lo:r:t:',
+            ['contents=', 'format=', 'hits=', 'limits', 'output=',
+             'regex=', 'time=', 'help'])
 
         for opt, arg in opts:
             if opt in ('-c', '--contents'):
-                urlContents = arg
-            elif opt in ('-h', '-hits'):
+                pathContents = arg
+            elif opt in ('-f', '--format'):
+                format = int(arg)
+                if 1 > format or format > 3:
+                    print ('format', format, 'out of range - setting to 1')
+                    format = 1
+            elif opt in ('-h', '--hits'):
                 hits = int(arg)
+            elif opt in ('-l', '--limits'):
+                limits = True
             elif opt in ('-o', '--output'):
                 output = open(arg, 'w')
                 outputSet = True
@@ -205,12 +239,12 @@ def main():
 
     data = {}
     for fileName in args:
-        data = parseLog(fileName, data, urlContents, regex)
-        if outputSet:
-            print ('Parsed:', fileName, len(data), file=output)
-        print ('Parsed:', fileName, len(data))
+        data = parseLog(fileName, data, pathContents, regex, format)
+        #if outputSet:
+        #    print ('Parsed:', fileName, len(data), file=output)
+        print ('Parsed:', fileName, len(data), file=sys.stderr)
 
-    reportData(data, output, hits, rptTime)
+    reportData(data, output, hits, rptTime, limits)
 
 if __name__ == '__main__':
     main()
