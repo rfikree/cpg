3925dbb36548db8ee416e45780878c743d188aa0
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2018-08-31 02:26:55 +0000

    Fix CPG_HOSTNAME lookup
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@11260 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- buildinfo/updateBuildInfo.sh
+++ buildinfo/updateBuildInfo.sh
@@ -2,7 +2,7 @@
 
 # Update here if JAVA or WebLogic is updated
 for JAVA_HOME in  $(ls -drt /cpg/3rdParty/installs/java/jdk1.7*); do
-	continue
+    continue
 done
 export JAVA_HOME
 export WL_HOME=/cpg/3rdParty/installs/Oracle/Middleware_Home1/wlserver_10.3
@@ -14,25 +14,25 @@ umask 022
 export CLASSPATH=${WL_HOME}/server/lib/weblogic.jar
 export PATH=${JAVA_HOME}/bin:${WL_HOME}/common/bin:${PATH}
 export WLST_PROPERTIES="-Dweblogic.security.TrustKeyStore=CustomTrust
-	-Dweblogic.security.CustomTrustKeyStoreFileName=/cpg/3rdParty/security/CPGTrust.jks
-	-Dweblogic.ThreadPoolPercentSocketReaders=75
-	-Dweblogic.ThreadPoolSize=32
-	-Dweblogic.security.SSL.enableJSSE=true
-	-Dweblogic.security.allowCryptoJDefaultJCEVerification=true
-	-Dweblogic.security.allowCryptoJDefaultPRNG=true
-	-Dweblogic.security.SSL.enableJSSE=true
-	-Dweblogic.ssl.JSSEEnabled=true
-	-Dweblogic.security.SSL.minimumProtocolVersion=TLSv1
-	-Dweblogic.security.allowCryptoJDefaultJCEVerification=true
-	-Dweblogic.security.allowCryptoJDefaultPRNG=true
-	-Dweblogic.security.SSL.ignoreHostnameVerification=true
-	-Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"
+    -Dweblogic.security.CustomTrustKeyStoreFileName=/cpg/3rdParty/security/CPGTrust.jks
+    -Dweblogic.ThreadPoolPercentSocketReaders=75
+    -Dweblogic.ThreadPoolSize=32
+    -Dweblogic.security.SSL.enableJSSE=true
+    -Dweblogic.security.allowCryptoJDefaultJCEVerification=true
+    -Dweblogic.security.allowCryptoJDefaultPRNG=true
+    -Dweblogic.security.SSL.enableJSSE=true
+    -Dweblogic.ssl.JSSEEnabled=true
+    -Dweblogic.security.SSL.minimumProtocolVersion=TLSv1
+    -Dweblogic.security.allowCryptoJDefaultJCEVerification=true
+    -Dweblogic.security.allowCryptoJDefaultPRNG=true
+    -Dweblogic.security.SSL.ignoreHostnameVerification=true
+    -Djdk.tls.client.protocols=TLSv1,TLSv1.1,TLSv1.2"
 
 # Determine the environment to run reports for
 PROFILE_DIR=/cpg/3rdParty/scripts/cpg/profiles
 CPG_ALIAS_LOOKUP_FILE=${PROFILE_DIR}/hostname.map
 CPG_HOSTNAME=$(egrep -i "^${HOSTNAME}," ${CPG_ALIAS_LOOKUP_FILE})
-CPG_HOSTNAME=${CPG_HOSTNAME##*,}
+CPG_HOSTNAME=$(echo ${CPG_HOSTNAME} | cut -d, -f2)
 CPG_ENV=${CPG_HOSTNAME%%-*}
 
 
@@ -41,6 +41,6 @@ $(dirname ${0})/buildReports.py -e ${CPG_ENV} &> buildReports.log
 
 # Copy reports to reporting server
 scp ${CPG_ENV}*.html \
-	optadm@cpowiki.cpggpc.ca:/cpg/cpo_apps/build-info/wipro &> scp.log
+    optadm@cpowiki.cpggpc.ca:/cpg/cpo_apps/build-info/wipro &> scp.log
 
 # EOF
