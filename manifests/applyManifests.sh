67a117224f9b78779d70a47163b9e36f77ba69a8
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2018-09-10 19:11:57 +0000

    Initial update for Solaris 12
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@11329 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- manifests/applyManifests.sh
+++ manifests/applyManifests.sh
@@ -10,122 +10,126 @@ export CPG_HOSTNAME=${CPG_HOSTNAME##*,}
 
 # Function to update or install configs only if missing or changed
 applyManifest() {
-	manifest_dir=${MANIFEST_BASE}/application/${1}
-	manifest_site=${MANIFEST_BASE}/site
-	manifest_src=${SOURCE_BASE}/${2}/${3}
-	manifest_dst=${manifest_site}/${4}
-	manifest_mv=${manifest_dir}/${4}
-	manifest_name=${4}
+    manifest_dir=${MANIFEST_BASE}/application/${1}
+    manifest_site=${MANIFEST_BASE}/site
+    manifest_src=${SOURCE_BASE}/${2}/${3}
+    manifest_dst=${manifest_site}/${4}
+    manifest_mv=${manifest_dir}/${4}
+    manifest_name=${4}
 
-	if [ ! -f ${manifest_src} ]; then
-		echo FATAL: Source file ${manifest_src} is missing
-		return 1
-	elif [ -f ${manifest_mv} ]; then
-		echo Need to move: ${manifest_mv}
-		cp ${manifest_src} /tmp/${manifest_name}
-		perl -pi -e "s|/site/|/${1}/|" /tmp/${manifest_name}
-		if ! diff ${manifest_dir}/${manifest_name} ${manifest_mv} >/dev/null; then
-			mv /tmp/${manifest_name} ${manifest_mv}
-			cd ${manifest_dir}
-			svccfg import ${manifest_name}
-			return 0
-		else
-			rm /tmp/${manifest_name}
-		fi
-		return 1
-	elif [ ! -f ${manifest_dst} ]; then
-		echo Installing: ${manifest_dst}
-		cp ${manifest_src} ${manifest_dst}
-	elif diff ${manifest_src} ${manifest_dst} >/dev/null; then
-		echo ${manifest_dst} is up to date
-		return 0
-	else
-		echo Updating: ${manifest_dst}
-		cp ${manifest_dst}{,.$(date +%Y%m%dT%H%M)}
-		cp ${manifest_src} ${manifest_dst}
-	fi
+    if [ ! -f ${manifest_src} ]; then
+        echo FATAL: Source file ${manifest_src} is missing
+        return 1
+    elif [ -f ${manifest_mv} ]; then
+        echo Need to move: ${manifest_mv}
+        cp ${manifest_src} /tmp/${manifest_name}
+        perl -pi -e "s|/site/|/${1}/|" /tmp/${manifest_name}
+        if ! diff ${manifest_dir}/${manifest_name} ${manifest_mv} >/dev/null; then
+            mv /tmp/${manifest_name} ${manifest_mv}
+            cd ${manifest_dir}
+            svccfg import ${manifest_name}
+            return 0
+        else
+            rm /tmp/${manifest_name}
+        fi
+        return 1
+    elif [ ! -f ${manifest_dst} ]; then
+        echo Installing: ${manifest_dst}
+        cp ${manifest_src} ${manifest_dst}
+    elif diff ${manifest_src} ${manifest_dst} >/dev/null; then
+        echo ${manifest_dst} is up to date
+        return 0
+    else
+        echo Updating: ${manifest_dst}
+        cp ${manifest_dst}{,.$(date +%Y%m%dT%H%M)}
+        cp ${manifest_src} ${manifest_dst}
+    fi
 
-	cd ${manifest_site}
-	if svccfg validate ${manifest_name}; then
-		svccfg import ${manifest_name}
-	else
-		echo ${manifest_name} failed to validate
-		return 1
-	fi
+    cd ${manifest_site}
+    if svccfg validate ${manifest_name}; then
+        svccfg import ${manifest_name}
+    else
+        echo ${manifest_name} failed to validate
+        return 1
+    fi
 }
 
 
 # Define the manifests to apply
 case ${CPG_HOSTNAME:-''} in
-        *-appadm)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanagerAppAdm.xml nodemanager.xml"
-                ;;
-        *-bdt)
-        		epa_manifest="epagent.xml"
-        		wl_manifest=""
-                ;;
-        *-blcpo)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanager1x.xml nodemanager.xml"
-                ;;
-        *-cpodeploy)
-        		epa_manifest="epagentCpodeploy.xml"                                               
-        		wl_manifest=""
-                ;;
-        *-soaz0)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanager5x.xml nodemanager.xml"
-                ;;
-        *-soaz1)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanager6x.xml nodemanager.xml"
-                ;;
-        *-uicpo)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanager1x.xml nodemanager.xml"
-                ;;
-        *-wladm)
-        		epa_manifest="epagent.xml"
-                wl_manifest="weblogicAdmin.xml weblogic.xml"
-                ;;
-        *-ws)
-        		epa_manifest="epagent.xml"
-                wl_manifest="nodemanager3x.xml nodemanager.xml"
-                ;;
-        *)
-        		echo Unknown host ${HOSTNAME} ${CPG_HOSTNAME}
-        		exit 99
-        		;;
+    *-appadm)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanagerAppAdm.xml nodemanager.xml"
+        ;;
+    *-bdt)
+        epa_manifest="epagent.xml"
+        wl_manifest=""
+        ;;
+    *-blcpo)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager1x.xml nodemanager.xml"
+        ;;
+    *-blwscpo)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager1x.xml nodemanager.xml"
+        ;;
+    *-cpodeploy)
+        epa_manifest="epagentCpodeploy.xml"
+        wl_manifest=""
+        ;;
+    *-soaz0)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager5x.xml nodemanager.xml"
+        ;;
+    *-soaz1)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager6x.xml nodemanager.xml"
+        ;;
+    *-uicpo)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager1x.xml nodemanager.xml"
+        ;;
+    *-wladm)
+        epa_manifest="epagent.xml"
+        wl_manifest="weblogicAdmin.xml weblogic.xml"
+        ;;
+    *-ws)
+        epa_manifest="epagent.xml"
+        wl_manifest="nodemanager3x.xml nodemanager.xml"
+        ;;
+    *)
+        echo Unknown host ${HOSTNAME} ${CPG_HOSTNAME}
+        exit 99
+        ;;
 esac
 
 
 # Apply the standard manifests
 if [[ ${CPG_HOSTNAME%%-*} != dev ]]; then
-	applyManifest introscope general ${epa_manifest} "epagent.xml"
+    applyManifest introscope general ${epa_manifest} "epagent.xml"
 fi
 if [[ -n ${wl_manifest} ]]; then
-	applyManifest weblogic ${CPG_HOSTNAME%%-*} ${wl_manifest}
+    applyManifest weblogic ${CPG_HOSTNAME%%-*} ${wl_manifest}
 fi
 
-# Apply test manifest 
+# Apply test manifest
 if [[ ! -f /usr/local/bin/olctest ]]; then
-	cp ${SOURCE_BASE}/general/olctest /usr/local/bin
+    cp ${SOURCE_BASE}/general/olctest /usr/local/bin
 elif ! diff ${SOURCE_BASE}/general/olctest /usr/local/bin/olctest >/dev/null; then
-	cp ${SOURCE_BASE}/general/olctest /usr/local/bin
-fi 
+    cp ${SOURCE_BASE}/general/olctest /usr/local/bin
+fi
 applyManifest olctest general olctest.xml olctest.xml
 
 # Cleanup olc_services
 for f in  /etc/rc?.d/*olc_services /etc/init.d/olc_services; do
-	if [ -e ${f} ]; then
-		echo Removing ${f}
-		rm ${f}
-	fi
-	if [ -L ${f} ]; then
-		echo Removing ${f}
-		rm ${f}
-	fi
+    if [ -e ${f} ]; then
+        echo Removing ${f}
+        rm ${f}
+    fi
+    if [ -L ${f} ]; then
+        echo Removing ${f}
+        rm ${f}
+    fi
 done
 
 
