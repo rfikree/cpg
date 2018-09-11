c7775132cb42a80dd6ed6800fa767b3eed46c6fc
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2018-09-11 19:54:06 +0000

    Remove temporary prefixes from host names
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@11360 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- manifests/applyManifests.sh
+++ manifests/applyManifests.sh
@@ -5,8 +5,9 @@ SOURCE_BASE=/cpg/3rdParty/scripts/cpg/manifests
 
 PROFILE_DIR=/cpg/3rdParty/scripts/cpg/profiles
 CPG_ALIAS_LOOKUP_FILE=${PROFILE_DIR}/hostname.map
-CPG_HOSTNAME=$(egrep -i "^$(hostname)," ${CPG_ALIAS_LOOKUP_FILE})
-export CPG_HOSTNAME=$(echo ${CPG_HOSTNAME} | cut -d, -f2)
+export CPG_HOSTNAME=$(egrep    -i "^$(hostname)," ${CPG_ALIAS_LOOKUP_FILE})
+CPG_HOSTNAME=$(echo ${CPG_HOSTNAME} | cut -d, -f2)
+CPG_HOSTNAME=${CPG_HOSTNAME#?-}
 
 # Function to update or install configs only if missing or changed
 applyManifest() {
