23240766cb276a185752dba757cef90ab85bd44f
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2018-11-23 22:38:15 +0000

    Locate latest java version correctly
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@11821 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- resetContent.sh
+++ resetContent.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 URL_PATH='/web/iw/admin/JCS.jsp?action=clearAllRegions'
-                  
+
 export CLASSPATH=/cpg/3rdParty/scripts/cpg/testing
-for JAVA_BIN in /cpg/3rdParty/installs/java/jdk1*/bin; do continue; done
+for JAVA_BIN in $(ls -drt /cpg/3rdParty/installs/java/jdk1*/bin); do continue; done
 PATH=$JAVA_BIN:$PATH
 
 if [[ ${CPG_HOSTNAME} != prd-cpodeploy ]]; then
-	echo
-	echo This script only runs on prd-cpodeploy
-	echo
-	exit 1
+    echo
+    echo This script only runs on prd-cpodeploy
+    echo
+    exit 1
 fi
 
 cat <<EOF
@@ -30,13 +30,13 @@ EOF
 sleep 30
 
 for ms in {1..6}; do
-	for host in prd-a-uicpo prd-b-uicpo prd-c-uicpo; do
-		for stack in 10 11; do
-			port=${stack}30${ms}
-			java URLReader http://${host}.cpggpc.ca:${port}${URL_PATH}
-			sleep 20
-		done
-	done
+    for host in prd-a-uicpo prd-b-uicpo prd-c-uicpo; do
+        for stack in 10 11; do
+            port=${stack}30${ms}
+            java URLReader http://${host}.cpggpc.ca:${port}${URL_PATH}
+            sleep 20
+        done
+    done
 done
 
 # EOF
