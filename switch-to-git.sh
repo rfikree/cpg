f7950fd7a1c6fe781cc8989816d9fbad26297bc5
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2019-07-10 17:40:22 +0000

    Automate switch from SVN to GIT
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@12487 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

---
+++ switch-to-git.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+DIRECTORY=${1:-''}
+URL_BASE='MYYUX://wvzE-nq7-1JGQLNH-nob:vAKEvjDq%xz@LNY.HULLUH.HF'
+URL_BASE=$(echo ${URL_BASE} | tr 'A-Z0-9a-z' '5-9a-zA-Z0-4')
+GIT_URL_BASE=${URL_BASE}/scm/devcoe-weblogic-automation
+GIT_URL_BASE=${2:-$GIT_URL_BASE}
+
+usage() {
+    if [[ -n "${@}" ]]; then
+        echo
+        echo ${@}
+    fi
+
+    cat <<EOF
+
+Usage: ${0} [repository] [repository_url_base]
+
+    repository: Name of the repository to be pulled. It will be pulled
+        to a sub-directory of the same name.
+    repository_url_base: URL path up to the repository name.
+
+    Replaces directory checked out from SVN with the same directory
+    checked out from GIT. Run this script from the parent directory
+    of the directory checked out from SVN. The old directory will be
+    renamed with a ".svn" suffix after the directory is cloned from GIT.
+
+    If the specified directory does not exist, it will
+    be pulled from GIT and no SVN related activities will occur.
+
+EOF
+    exit 99
+}
+
+# Remove configuraton files for other stacks and enviroments
+trim_automation() {
+    # Skip if we aren't running as service account
+    if ! [[ ${LOGNAME} =~ ^(dev|loc|stg|prd)[1-5][0-9]$ ]]; then
+        return
+    fi
+
+    echo
+    echo INFO: Trimming stack configuration tree
+    echo
+
+    cd automation/stacks
+    PREFIX=a${CPG_LOGNAME:3:1}${CPG_LOGNAME:0:1}
+    #echo PREFIX ${PREFIX}
+
+    for dir in *; do
+        if ! [[ ${dir} =~ ${PREFIX}[1-5][0-9] ]]; then
+            echo Removing configuration for: ${dir}
+            git update-index --assume-unchanged ${dir}/*/*
+            rm ${dir}/*/*
+        fi
+    done
+
+    find . -depth -empty -delete
+    echo
+}
+
+# Check for and parse the specified directory
+if [[ -z ${DIRECTORY} ]]; then
+    usage
+fi
+DIRECTORY=$(python -c "import os,sys; print os.path.abspath(sys.argv[1])" ${DIRECTORY})
+PARENT=$(dirname ${DIRECTORY})
+REPO_DIR=$(basename ${DIRECTORY})
+
+
+# Ensure we have GIT installed
+if ! which git >/dev/null; then
+    echo
+    echo WARN: Unable to locate git
+    echo
+fi
+
+
+# Check the parent directory
+if [[ -z ${PARENT} ]]; then
+    usage
+elif [[ ! -d ${PARENT} ]]; then
+    usage FATAL: Directory ${PARENT} was not found
+elif [[ ! -w ${PARENT} ]]; then
+    usage FATAL: Ubable to update ${PARENT}
+else
+    cd ${PARENT}
+
+fi
+
+
+# Setup clone command
+if ! [[ ${CPO_HOSTNAME:-''} =~ (stg|prd)-cpodeploy ]]; then
+    echo INFO: Cloning dev branch
+    CLONE='git clone -q --branch dev'
+else
+    CLONE='git clone -q --branch master'
+fi
+
+
+# Process the requested directory, if possible
+if [[ ! -d ${REPO_DIR} ]]; then
+    echo INFO: Doing initial checkout of ${REPO_DIR}
+    ${CLONE} ${GIT_URL_BASE}/${REPO_DIR}.git ./${REPO_DIR}
+    chmod go-rwx ${REPO_DIR}/.git ${REPO_DIR}/.git/config
+
+elif [[ -d ${REPO_DIR}/.git ]]; then
+    echo INFO: Directory ${REPO_DIR} has already been updated
+    exit
+
+elif [[ -d ${REPO_DIR}/.svn ]]; then
+    echo
+    echo INFO: Replacing ${REPO_DIR} with version from GIT
+
+    if ${CLONE} ${GIT_URL_BASE}/${REPO_DIR}.git ./${REPO_DIR}.new; then
+        mv ${REPO_DIR}{,.svn}
+        mv ${REPO_DIR}{.new,}
+        chmod go-rwx ${REPO_DIR}/.git ${REPO_DIR}/.git/config
+    else
+        echo
+        echo FATAL: Initial pull of ${REPO_DIR} failed
+        exit
+    fi
+    echo
+
+else
+    usage FATAL: ${REPO_DIR} is not under configuration management
+    exit
+fi
+
+if [[ ${REPO_DIR} = automation ]]; then
+    trim_automation
+fi
+
+
+# EOF
---
+++ update-from-cm.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -u
+
+SCRIPT=$(python -c "import os,sys; print os.path.abspath(sys.argv[1])" ${0})
+DIRECTORY=${1:-''}
+if [[ -n ${2:-''} ]]; then
+    SUMFILE=$(python -c "import os,sys; print os.path.abspath(sys.argv[1])" ${2})
+fi
+
+usage() {
+    if [[ -n "${@}" ]]; then
+        echo
+        echo ${@}
+    fi
+
+    cat <<EOF
+
+Usage: ${0} [directoty [script-to-rub]
+
+Updates the-directory from the configuration manager it uses one.
+
+EOF
+    exit
+}
+
+# Ensure we have GIT installed
+if ! which git >/dev/null; then
+    echo
+    echo WARN: Unable to locate git
+    echo
+    exit
+fi
+
+# Checksum the script, if specified, before we update.
+if [[ -n ${SUMFILE:-''} ]]; then
+    # Ensure we have CkSum installed
+    if ! which cksum >/dev/null; then
+        echo
+        echo WARN: Unable to locate cksum
+        echo
+        exit
+    fi
+
+    CHKSUM_1=$(cksum ${SCRIPT})
+fi
+
+# Ensure the directory is updatable
+if [[ -z ${DIRECTORY} ]]; then
+    usage
+elif [[ ! -d ${DIRECTORY} ]]; then
+    usage FATAL: Directory ${DIRECTORY} not found
+elif [[ ! -w ${DIRECTORY} ]]; then
+    usage FATAL: Ubable to update ${DIRECTORY}
+else
+    cd ${DIRECTORY}
+fi
+
+# Do the update and display the status
+if [[ -d .git ]]; then
+    git pull
+    git status -s
+elif [[ -d .svn ]]; then
+    cd ..
+    SCRIPT_DIR=$(dirname ${SCRIPT})
+    ${SCRIPT_DIR}/switch-to-git.sh ${DIRECTORY}
+else
+    usage WARNING: Directory ${DIRECTORY} is not using a configuration manager
+fi
+
+# Checksum the script, if specified, after the update.
+if [[ -n ${SUMFILE:-''} ]]; then
+   cd -
+   CHKSUM_2=$(cksum ${SCRIPT})
+   [[ ${CHKSUM_1} == ${CHKSUM_2} ]]
+   exit $?
+fi
+
+# EOF
