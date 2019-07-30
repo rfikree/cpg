a5afdf14024f9f35ce5dd198aaa19db703795119
Bill Thorsteinson <Bill.Thorsteinson@innovapost.com>
2019-07-30 19:33:42 +0000

    Remove old crontab from script
    git-svn-id: http://cposvn.cpggpc.ca/configuration_repo/automation/trunk/cpg@12652 fcc38d2d-2c38-4090-bbf2-7fab0ba27683

--- utils/migrate_repos.sh
+++ utils/migrate_repos.sh
@@ -26,18 +26,3 @@ exit
 
 # EOF
 
-
-[root@l-03382-c5a ~]# crontab -l -u teamcity
-# ===========================================================
-# rsync release repo from staging to prd cpodeploy:/cpg/repos, Bob Ritchie
-#
-6,16,26,36,46,56 * * * * /usr/bin/rsync -avz --delete 10.237.116.162:/cpg/repos/maven/release_repo/ /cpg/repos/maven/release_repo/ >> /$HOME/rsync.logs/rsync-log.`date +\%Y\%m\%d\%H\%M` 2>&1
-#
-# remove rsync.logs over 5 days old
-29 19 * * * /usr/bin/find $HOME/rsync.logs/rsync* -ctime +5 -exec rm {} \;
-#
-# remove rsync.logs where there were no files updated
-35 20 * * * /usr/bin/find $HOME/rsync.logs/rsync* -size  -155c -exec rm {} \;
-#
-# ===========================================================
-
